a82da20306d6096f6b13d6a201c2c32695511c75
eshiett1995 <eshiett1995@gmail.com>
2020-06-18 11:26:38 +0100

    adding test UI

--- okra_widget/src/main/java/com/okra/widget/utils/NetworkUtils.java
+++ okra_widget/src/main/java/com/okra/widget/utils/NetworkUtils.java
@@ -1,4 +1,42 @@
 package com.okra.widget.utils;
 
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class NetworkUtils {
+
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    public String get(String url){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try{
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        }catch (Exception ex){
+            return url;
+        }
+    }
+
+    String post(String url, String json) {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        }catch (Exception ex){
+            return url;
+        }
+    }
 }
