90176ab012ae91b3ac7b7fd61053c635365b0e39
eshiett1995 <eshiett1995@gmail.com>
2021-01-14 09:09:17 +0100

    wip

--- okra_widget/src/main/java/com/okra/widget/activity/OkraWebActivity.java
+++ okra_widget/src/main/java/com/okra/widget/activity/OkraWebActivity.java
@@ -68,7 +68,7 @@ public class OkraWebActivity extends AppCompatActivity {
         webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         okraLinkWebview.addJavascriptInterface(new WebInterface(this), "Android");
 
-        okraLinkWebview.loadUrl("https://dev-v2-app.okra.ng/mobile.html");
+        okraLinkWebview.loadUrl("https://80f687004f88.ngrok.io/mobile.html");  //https://dev-v2-app.okra.ng
 
         okraLinkWebview.setWebViewClient(new WebViewClient() {
             @Override
