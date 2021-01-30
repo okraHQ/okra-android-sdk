ea5a572135451b132002664b32d2b987f5d40d8b
Bolu Okunaiya <boluokunaiya@gmail.com>
2021-01-30 08:45:12 +0100

    Return Data Object Along with string

--- okra_widget/src/main/java/com/okra/widget/utils/WebInterface.java
+++ okra_widget/src/main/java/com/okra/widget/utils/WebInterface.java
@@ -24,13 +24,23 @@ public class WebInterface {
     @JavascriptInterface
     public void onSuccess(String json) {
         OkraHandler.data = json;
+        formatJsonString(json);
         OkraHandler.isSuccessful = true;
         OkraHandler.isDone = true;
         OkraHandler.hasError = false;
     }
 
+    private void formatJsonString(String json) {
+        try {
+            OkraHandler.dataObject = FormatJson.formatJson(json);
+        } catch (Exception exception) {
+            OkraHandler.dataObject = null;
+        }
+    }
+
     @JavascriptInterface
     public void onError(String json) {
+        formatJsonString(json);
         OkraHandler.data = json;
         OkraHandler.hasError = true;
         OkraHandler.isDone = true;
---
+++ okra_widget/src/main/java/com/okra/widget/utils/successmodel/OkraResponse.kt
@@ -0,0 +1,27 @@
+package com.okra.widget.utils.successmodel
+
+import java.io.Serializable
+
+data class OkraResponse (
+
+		val done : Boolean?,
+		val record_id : String?,
+		val record : String?,
+		val bank_id : String?,
+		val customer_id : String?,
+		val balance : Balance?,
+		val transactions : Transactions?,
+		val guarantors : List<String>?,
+		val redirect_url : String?,
+		val callback_url : String?,
+		val launchAgain : Boolean?,
+		val hideExit : String?,
+		val success_title : String?,
+		val success_message : String?,
+		//val options : Options?,
+		val directors : String?,
+		val auth : Auth?,
+		val identity : Identity?,
+		val income : Income?,
+		val accounts : List<Accounts>?
+): Serializable
