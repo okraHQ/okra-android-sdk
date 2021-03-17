329d4be819446e5a2f99ff3ea736fc5e7a24191f
Bolu Okunaiya <boluokunaiya@gmail.com>
2021-03-17 13:15:26 +0100

    Change to production url

--- okra_widget/src/main/java/com/okra/widget/activity/OkraWebActivity.java
+++ okra_widget/src/main/java/com/okra/widget/activity/OkraWebActivity.java
@@ -79,7 +79,7 @@ public class OkraWebActivity extends AppCompatActivity {
         webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         okraLinkWebview.addJavascriptInterface(new WebInterface(this), "Android");
 
-        okraLinkWebview.loadUrl("https://30907dcdcb2b.ngrok.io/mobile.html");  //
+        okraLinkWebview.loadUrl("https://v2-mobile.okra.ng/");  //
 
         okraLinkWebview.setWebViewClient(new WebViewClient() {
             @Override
